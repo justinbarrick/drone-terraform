a93efdf50be5e02f9154661790074733c69f5206
Your Name <you@example.com>
2018-03-14 05:20:11 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+Terraform plugin for Drone.
+
+Allowed variables:
+
+* `root`: the terraform directory.
+* `apply`: if true, apply the changes, otherwise just plan.
+* `slack_url`: Slack URL to submit Terraform summary to.
+
+Example `.drone.yml`, with recommended branching flow using [drone-git-push](https://github.com/appleboy/drone-git-push) to manage the state:
+
+```
+pipeline:
+  terraform_apply:
+    image: justinbarrick/drone-terraform:2018.3.3
+    root: terraform
+    apply: "true"
+    slack_url: SLACK_URL
+    when:
+      branch: master
+      event: push
+
+  terraform_push_state:
+    image: appleboy/drone-git-push
+    branch: master
+    remote_name: origin
+    commit_message: "Applying terraform state updates. [skip ci]"
+    force: false
+    commit: true
+    secrets: [ GIT_PUSH_SSH_KEY ]
+    when:
+      branch: master
+      event: push
+
+  terraform_plan:
+    image: justinbarrick/drone-terraform:2018.3.3
+    root: terraform
+    slack_url: SLACK_URL
+    when:
+      event: push
+
+  terraform_push_plan:
+    image: appleboy/drone-git-push
+    branch: ${DRONE_BRANCH}
+    remote_name: origin
+    commit_message: "Terraform plan update. [skip ci]"
+    force: false
+    commit: true
+    secrets: [ GIT_PUSH_SSH_KEY ]
+    when:
+      event: push
+
+  slack:
+    image: plugins/slack
+    webhook: SLACK_URL
+    channel: '#kubernetes'
+    when:
+      status: [success, failure]
+      event: push
+    template: >
+      {{#success build.status}}
+        Terraform build <{{build.link}}|#{{build.number}}> for <https://github.com/{{repo.owner}}/{{repo.name}}/tree/{{build.commit}}|{{repo.name}}#{{build.branch}}> by {{build.author}} succeeded!
+      {{else}}
+        Terraform build <{{build.link}}|#{{build.number}}> for <https://github.com/{{repo.owner}}/{{repo.name}}/tree/{{build.commit}}|{{repo.name}}#{{build.branch}}> by {{build.author}} failed.
+      {{/success}}
+```
---
+++ run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+PLAN_OUTPUT="${PLUGIN_TERRAFORM_PLAN_OUTPUT:-terraform.plan}"
+
+function slack_notify {
+    MESSAGE=$(terraform show -no-color $PLAN_OUTPUT |grep -v '^ ')
+
+    ADDITIONS=$(echo $MESSAGE |grep -cE "^[+] ")
+    CHANGES=$(echo $MESSAGE |grep -cE "^[+-]/[-+] ")
+    DESTROYED=$(echo $MESSAGE |grep -cE "^[-] ")
+
+    if [ ${DESTROYED:-0} -gt 0 ]; then
+        COLOR="#db1515"
+    elif [ ${CHANGES:-0} -gt 0 ]; then
+        COLOR="#e2df06"
+    elif [ ${ADDITIONS:-0} -gt 0 ]; then
+        COLOR="#2eb886"
+    else
+        COLOR="#4f4e49"
+        MESSAGE="No changes detected!"
+    fi
+
+    curl -so /dev/null "$PLUGIN_SLACK_URL" --data "$(echo $MESSAGE |jq -Rs --arg apply "$PLUGIN_APPLY" --arg drone_build "$DRONE_BUILD_LINK" --arg color "$COLOR" \
+      --arg add ${ADDITIONS:-0} --arg rm ${DESTROYED:-0} --arg changed ${CHANGES:-0} '{
+        "channel":"#kubernetes",
+        "icon_emoji": ":terraform:",
+        "username": (if $apply == "true" then "Terraform Deployer" else "Terraform Planner" end),
+        "attachments": [
+            {
+                "color": $color,
+                "title": ((if $apply == "true" then "Deployed" else "Planned" end) + " terraform infrastructure changes"),
+                "title_link": $drone_build,
+                "text": ((if $apply == "true" then "*Deployed:* \($add) added, \($changed) changed, \($rm) destroyed:" else "*Plan:* \($add) to add, \($changed) to change, \($rm) to destroy:" end) + "\n\n" + .),
+            }
+        ]
+    }')"
+}
+
+if [ ! -z "$PLUGIN_ROOT" ]; then
+    cd "$PLUGIN_ROOT"
+fi
+
+if [ "$PLUGIN_APPLY" == "true" ]; then
+    set -e
+    terraform apply -input=false "${PLAN_OUTPUT}" ${PLUGIN_TERRAFORM_ARGS:-}
+    set +e
+
+    slack_notify
+    rm "${PLAN_OUTPUT}"
+else
+    terraform plan -input=false -out="${PLAN_OUTPUT}" -detailed-exitcode ${PLUGIN_TERRAFORM_ARGS:-}
+    EXIT_STATUS=${PIPESTATUS[0]}
+
+    if [ $EXIT_STATUS -eq 0 ]; then
+        echo "No changes detected, reseting plan output."
+        git checkout "${PLAN_OUTPUT}"
+    elif [ $EXIT_STATUS -eq 2 ]; then
+        echo "Changes detected."
+    else
+        echo "Failing build due to terraform plan failure."
+        exit 1
+    fi
+
+    slack_notify
+fi
